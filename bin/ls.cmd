b9109335447883eb80b4cf5e6adb2f90abb6839c
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2016-11-27 15:35:00 +0000

    update; ls to filter unwanted text

--- bin/ls.cmd
+++ bin/ls.cmd
@@ -1,2 +1,10 @@
-@dir /w /og %*
+@dir /w /og %* | ^
+find /v "Volume in drive" | ^
+find /v "Volume Serial Number" | ^
+find /v "File(s)" | ^
+find /v "Dir(s)" | ^
+find /v "Directory of" | ^
+findstr /v "^$"
+
+@echo.
 
