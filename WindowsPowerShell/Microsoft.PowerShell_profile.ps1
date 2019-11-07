04a3b11dfdc0c90613827875c323f9ddfdc1624a
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2019-11-07 11:10:45 +0000

    add; powershell aliases & import JumpTo

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,29 @@
-#Write-Output "Current user, Current Host"
-#Write-Output "Hello from my profile!"
+Write-Output "Current user, Current Host"
+Write-Output "Hello from my profile!"
+
+Import-Module JumpTo
+
+function Open-CurrentDirectory {
+    ii .
+}
+
+function Exit-Powershell {
+    exit
+}
+
+set-alias l ls
+set-alias q Exit-Powershell
+set-alias :q Exit-Powershell
+set-alias e Open-CurrentDirectory
+set-alias v vim
+
+
+set-alias j Set-LocationByAlias
+set-alias ja Add-LocationAlias
+set-alias je Edit-LocationAliases
+set-alias jls Get-LocationAliases
+
+function gs {
+    git status
+}
 
