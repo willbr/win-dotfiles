65e0f0063c276ae7b8228d4f1894dfdb2d99791b
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2019-12-04 20:15:31 +0000

    add; autoruns

--- ps-build.ps1
+++ ps-build.ps1
@@ -12,3 +12,13 @@ New-HardLink  $PROFILE.CurrentUserCurrentHost ".\WindowsPowerShell\Microsoft.Pow
 
 New-HardLink $PROFILE.CurrentUserAllHosts ".\WindowsPowerShell\profile.ps1"
 
+$path_run = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
+
+# Set-Itemproperty -path $path_run -Name 'Notepad' -value 'c:\windows\notepad.exe'
+
+$path_hotkeys = Resolve-Path '.\scripts\hotkeys.ahk'
+Set-Itemproperty -path $path_run -Name "Will's Hotkeys" -value """$path_hotkeys"""
+
+$path_hotstrings = Resolve-Path '.\scripts\hotstrings.ahk'
+Set-Itemproperty -path $path_run -Name "Will's Hotstrings" -value """$path_hotstrings"""
+
