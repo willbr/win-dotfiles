1e7933a5b44d91e50a185a23d065bc3fba4a2586
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2019-12-05 10:49:52 +0000

    add modules link

--- ps-build.ps1
+++ ps-build.ps1
@@ -12,13 +12,12 @@ New-HardLink  $PROFILE.CurrentUserCurrentHost ".\WindowsPowerShell\Microsoft.Pow
 
 New-HardLink $PROFILE.CurrentUserAllHosts ".\WindowsPowerShell\profile.ps1"
 
-$path_run = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
+New-Item -ItemType SymbolicLink -Path "$Home\Documents\WindowsPowerShell\Modules" -Target ".\WindowsPowerShell\Modules"
 
-# Set-Itemproperty -path $path_run -Name 'Notepad' -value 'c:\windows\notepad.exe'
+$path_run = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
 
 $path_hotkeys = Resolve-Path '.\scripts\hotkeys.ahk'
 Set-Itemproperty -path $path_run -Name "Will's Hotkeys" -value """$path_hotkeys"""
 
 $path_hotstrings = Resolve-Path '.\scripts\hotstrings.ahk'
 Set-Itemproperty -path $path_run -Name "Will's Hotstrings" -value """$path_hotstrings"""
-
