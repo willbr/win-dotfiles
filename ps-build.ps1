5f472aba34858c2a927a2b069b98ebaebfbd3b30
Will Bettridge-Radford <william.bettridge.radford@gmail.com>
2020-04-11 13:18:32 +0100

    tidy; build script add; vimfiles link

--- ps-build.ps1
+++ ps-build.ps1
@@ -1,18 +1,43 @@
 function New-HardLink() {
     Param ([string]$link, [string]$target)
     if (Test-Path $link) {
-        Write-Error "Error: $link already exists"
+        Write-Host -ForegroundColor Red "Error: $link already exists"
     } else {
         New-Item -ItemType HardLink -Path $link -Target $Target
     }
 }
 
+function New-SymbolicLink() {
+    Param ([string]$link, [string]$target)
+    if (Test-Path $link) {
+        Write-Host -ForegroundColor Red "Error: $link already exists"
+    } else {
+        New-Item -ItemType SymbolicLink -Path $path_vim -Target ".\vimfiles"
+    }
+}
+
+function Create-Missing-Directory() {
+    Param ([string]$path)
+    if (!(Test-Path $path)) {
+        New-Item -ItemType Directory -Path $path
+    }
+}
+
+$path_profile = (Get-Item $PROFILE.CurrentUserCurrentHost).Directory.FullName
+
+Create-Missing-Directory $path_profile
+
 
 New-HardLink  $PROFILE.CurrentUserCurrentHost ".\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 
+$path_vim = Resolve-Path "~\vimfiles"
+New-SymbolicLink $path_vim ".\vimfiles"
+
 New-HardLink $PROFILE.CurrentUserAllHosts ".\WindowsPowerShell\profile.ps1"
 
-New-Item -ItemType SymbolicLink -Path "$Home\Documents\WindowsPowerShell\Modules" -Target ".\WindowsPowerShell\Modules"
+
+$path_modules = (Join-Path $path_profile "Modules")
+New-SymbolicLink $path_modules ".\WindowsPowerShell\Modules"
 
 $path_run = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
 
@@ -21,3 +46,4 @@ Set-Itemproperty -path $path_run -Name "Will's Hotkeys" -value """$path_hotkeys"
 
 $path_hotstrings = Resolve-Path '.\scripts\hotstrings.ahk'
 Set-Itemproperty -path $path_run -Name "Will's Hotstrings" -value """$path_hotstrings"""
+
